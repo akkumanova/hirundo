4d46d5c62d6d19f0d19cdc80812b607fdaaedc7d
Albena Kumanova <akumanova@yahoo.com>
2013-12-14 19:10:17 +0200

    Reorganized states

--- Hirundo/Hirundo/App/js/home/home.js
+++ Hirundo/Hirundo/App/js/home/home.js
@@ -7,5 +7,24 @@
     'ui.bootstrap',
     'navigation',
     'home.templates'
-  ]);
+  ]).config(['$stateProvider', function ($stateProvider) {
+    $stateProvider
+      .state({
+        name: 'rootHome',
+        url: '/home',
+        parent: 'root',
+        'abstract': true
+      })
+      .state({
+        name: 'home',
+        parent: 'rootHome',
+        url: '',
+        views: {
+          'pageView@root': {
+            templateUrl: 'home/templates/home.html',
+            controller: 'home.HomeCtrl'
+          }
+        }
+      });
+  }]);
 }(angular));
